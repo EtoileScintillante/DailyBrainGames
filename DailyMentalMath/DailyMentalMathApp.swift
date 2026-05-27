48233dfc651868f0ddf3fad3356eb0cddd470aa2
Esma <97445881+EtoileScintillante@users.noreply.github.com>
2026-05-27 22:27:52 +0200

    Initial Commit

---
+++ DailyMentalMath/DailyMentalMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyMentalMathApp.swift
+//  DailyMentalMath
+//
+//  Created by Esma Zuurbier on 27/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct DailyMentalMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyMentalMathTests/DailyMentalMathTests.swift
@@ -0,0 +1,19 @@
+//
+//  DailyMentalMathTests.swift
+//  DailyMentalMathTests
+//
+//  Created by Esma Zuurbier on 27/05/2026.
+//
+
+import Testing
+@testable import DailyMentalMath
+
+struct DailyMentalMathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
